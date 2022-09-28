4f032f5658258b1e9df16dc6d5741eb09a0dfe8e
daniel sepulveda <75953572+exteruz@users.noreply.github.com>
2022-09-28 08:48:47 -0500

    Update Minisia.java

--- logica/Minisia.java
+++ logica/Minisia.java
@@ -1,5 +1,6 @@
 
 package logica;
+import java.util.Scanner;
 
 import interfaz.Funciones;
 import Datos.Profesor;
@@ -11,12 +12,64 @@ public class Minisia {
     /**
      * @param args the command line arguments
      */
+ 
+    
     public static void main(String[] args) {
-        Funciones.inicio();
-        Funciones.registrar_estudiante();
-        Nota nota = new Nota(2,1);
-        Nota nota2 = new Nota(2,1);
-        Estudiante estudiante = new Estudiante(<nota>,2,"a","b","c","aa");
+       Estudiante[] estudiante_lista = {null,null,null,null,null};
+        for (int i = 0; i < estudiante_lista.length; i++) {
+            String[] nombre = {"valentina","samuel","daniel","rafael","alexei"};
+            String[] apellido = {"Pereira","rodriguez","martinez","barbosa","sanchez"};
+             String usuario = nombre[i].substring(0,2) + apellido;
+            estudiante_lista[i] =  new Estudiante(i,nombre[i],apellido[i],usuario,"derecho");     
+        }
+        
+        for (int i = 0; i < estudiante_lista.length; i++) {
+            System.out.println(estudiante_lista[i]);            
+            
+        }
+        
+        Scanner lectura = new Scanner(System.in);
+
+       
+        System.out.println("Bienvenido al sia");
+        System.out.println("Si usted se encuentra registrado  1 si desea registrarse 2");
+        int registro = lectura.nextInt();
+        
+        if(registro == 1 ){
+            System.out.println("Por favor ingrese su usuario");
+            String prueba = lectura.nextLine();
+            String registro_usuario = lectura.nextLine();
+        
+            
+
+         
+
+            
+            if(Funciones.comprobar_usuario(registro_usuario, estudiante_lista)){
+                Funciones.menu_estudiante();
+            
+            }else{
+                System.out.println("No se encuentra registrado");
+                
+            
+            
+            
+ 
+            
+        
+            }}else if(registro == 2 ){
+                    Estudiante estudiante = Funciones.registrar_estudiante();
+                    System.out.println(estudiante.toString());
+                    
+
+        
+        }else{
+            System.out.println("No se ha encontrado el numero");
+        }
+        
+
+        
+        
 
 
     }
