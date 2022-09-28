67cb2b6b61d9574e51eeb932853acebb07530ba9
daniel sepulveda <75953572+exteruz@users.noreply.github.com>
2022-09-28 00:08:02 -0500

    Create Nota.java

---
+++ Nota.java
@@ -0,0 +1,46 @@
+package Datos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Nota {
+    private float valor;
+    private int id;
+
+
+    public Nota(float valor, int id) {
+        this.valor = valor;
+        this.id = id;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Nota{" + "valor=" + valor + ", id=" + id + '}';
+    }
+    
+    
+    
+    
+}
