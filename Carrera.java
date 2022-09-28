53cb2ed398a5d8719c670285e93be7387ed0a49c
daniel sepulveda <75953572+exteruz@users.noreply.github.com>
2022-09-28 00:04:47 -0500

    Update Carrera.java

--- Carrera.java
+++ Carrera.java
@@ -1,24 +1,92 @@
+package Datos;
 
-package logica;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
-import interfaz.Funciones;
-import Datos.Profesor;
-import Datos.Estudiante;
-import Datos.Nota;
+/**
+ *
+ * @author Estudiante
+ */
+public class Carrera {
+    
+    private int id;
+    private String nombre;
+    private Estudiante[] estudiantes;
+    private Curso[] cursos;
+    private Profesor[] profesores;
 
-public class Minisia {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Funciones.inicio();
-        Funciones.registrar_estudiante();
-        Nota nota = new Nota(2,1);
-        Nota nota2 = new Nota(2,1);
-        Estudiante estudiante = new Estudiante(<nota>,2,"a","b","c","aa");
-
-
+    public Carrera(int id, String nombre, Estudiante[] estudiantes, Curso[] cursos, Profesor[] profesores) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estudiantes = estudiantes;
+        this.cursos = cursos;
+        this.profesores = profesores;
     }
+
+    public Carrera(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Carrera(int id, String nombre, Estudiante[] estudiantes) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estudiantes = estudiantes;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Estudiante[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(Estudiante[] estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public Curso[] getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(Curso[] cursos) {
+        this.cursos = cursos;
+    }
+
+    public Profesor[] getProfesores() {
+        return profesores;
+    }
+
+    public void setProfesores(Profesor[] profesores) {
+        this.profesores = profesores;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrera{" + "id=" + id + ", nombre=" + nombre + ", estudiantes=" + estudiantes + ", cursos=" + cursos + ", profesores=" + profesores + '}';
+    }
+
+  
+    
+    
+
+    
     
 }
