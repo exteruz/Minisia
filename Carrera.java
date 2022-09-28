28ad0e274f3ea5d742bd57f8d945f63948122e6a
daniel sepulveda <75953572+exteruz@users.noreply.github.com>
2022-09-28 00:02:52 -0500

    Create Carrera.java

---
+++ Carrera.java
@@ -0,0 +1,24 @@
+
+package logica;
+
+import interfaz.Funciones;
+import Datos.Profesor;
+import Datos.Estudiante;
+import Datos.Nota;
+
+public class Minisia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Funciones.inicio();
+        Funciones.registrar_estudiante();
+        Nota nota = new Nota(2,1);
+        Nota nota2 = new Nota(2,1);
+        Estudiante estudiante = new Estudiante(<nota>,2,"a","b","c","aa");
+
+
+    }
+    
+}
